7ca293bd1bf1d98682556806e313023dfa072979
donmiguel <michael.gau@gmail.com>
2018-09-17 08:55:05 +0200

    corrected template path setting

--- engergieDigital/settings.py
+++ engergieDigital/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'engergieDigital.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'chartGenerator/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
